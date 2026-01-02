c25e09aa36d0bdcfbab7441c877f0ece3871d12e
yahya12213 <yazmmat@gmail.com>
2026-01-02 18:19:01 +0100

    Switch to dj_database_url for database configuration

--- WallPic/settings.py
+++ WallPic/settings.py
@@ -87,13 +87,12 @@ WSGI_APPLICATION = 'WallPic.wsgi.application'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import dj_database_url
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
-
 
 
 # Password validation
